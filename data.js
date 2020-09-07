f3cd7c9376a875fdc99aba5055d990bd3883f1a5
Guilherme Barbosa <64219293+RubberMass@users.noreply.github.com>
2020-09-07 19:19:39 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        id:"starter",
+        img:"https://app.rocketseat.com.br/static/media/Starter.64c237cc.svg",
+        title:"Torne-se um programador desejado no mercado com esses cursos gratuitos",
+        duration:"5 módulos",
+        price:"Material gratuito",
+        time:"240 minutos",
+        class:"80 aulas"
+    },
+    {
+        id:"launchbase",
+        img:"https://app.rocketseat.com.br/static/media/LaunchBase.08285320.svg",
+        title:" Domine programação do zero e tenha acesso às melhores oportunidades do mercado",
+        duration:"5 módulos",
+        price:"Curso pago",
+        time:"6 meses",
+        class:"328 aulas"
+    },
+    {
+        id:"gostack",
+        img:"https://app.rocketseat.com.br/static/media/GoStack.83a178a0.svg",
+        title:"Treinamento imersivo nas tecnologias mais modernas de desenvolvimento web e mobile.",
+        duration:"8 módulos",
+        price:"Curso pago",
+        time:"indeterminado",
+        class:"indeterminado"
+    }
+]
